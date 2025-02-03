75ec5a141f2a6b287e2d07787a1fed6cfdcc39e0
bartek <bartekkoxu@o2.pl>
2025-02-03 19:02:45 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const path = require("path");
+
+const app = express();
+const SECRET_KEY = "gfgfklgfkgfkgfkfgkjgfkjgfjkgfjkgjfkjgkdwjkdsa,mcd,smv,smfmdmf,mfd,samdlksakledwaoirjwoakeowakoepwakfdxlkflsdklfdklreksldelwklsdkfkdlkfldklrekore3owkfdslf;aq;l4kl;34klek4lkldfkldklfdkll";
+const ALLOWED_REFERRER = "https://ouo.io";
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/gateway", (req, res) => {
+    const referrer = req.get("Referer") || req.get("Referrer");
+
+    if (!referrer || !referrer.startsWith(ALLOWED_REFERRER)) {
+        return res.status(403).send("error");
+    }
+
+    const token = jwt.sign({ ip: req.ip }, SECRET_KEY, { expiresIn: "5m" });
+
+    res.render("gateway", { token });
+});
+
+app.get("/content", (req, res) => {
+    const token = req.query.token;
+    if (!token) return res.status(403).send("no access");
+
+    jwt.verify(token, SECRET_KEY, (err, decoded) => {
+        if (err) return res.status(403).send("invalid token");
+
+        res.render("content");
+    });
+});
+
+app.listen(5555, () => console.log("server started on http://localhost:5555"));
